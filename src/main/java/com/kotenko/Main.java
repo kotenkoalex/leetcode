ed8217018cae979823f3f435c68097e097dd29b9
Alex <akotenko@prezna.com>
2024-01-02 23:36:53 -0500

    0201L_first_tasks: added twoSum, isPalindrome

--- src/main/java/com/kotenko/Main.java
+++ src/main/java/com/kotenko/Main.java
@@ -1,7 +1,49 @@
 package com.kotenko;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int potential;
+        int targetFirst = 0;
+        int targetSecond = 0;
+
+        for (int element = 0; element < nums.length; element++) {
+            potential = target - nums[element];
+
+            for (int i = element + 1; i < nums.length; i++) {
+                if (potential == nums[i]) {
+                    targetFirst = element;
+                    targetSecond = i;
+                }
+            }
+        }
+        return new int[]{targetFirst, targetSecond};
+    }
+
+    public static boolean isPalindrome(int x) {
+        String[] number = Integer.toString(x).split("");
+
+        if (number.length == 1) {
+            return true;
+        } else if (number.length % 2 == 0) {
+            List<String> firstHalf = Arrays.asList(Arrays.copyOfRange(number, 0, number.length / 2));
+            List<String> secondHalfReversed = Arrays.asList(Arrays.copyOfRange(number, number.length / 2, number.length));
+            Collections.reverse(secondHalfReversed);
+            return firstHalf.equals(secondHalfReversed);
+        } else {
+            List<String> firstHalf = Arrays.asList(Arrays.copyOfRange(number, 0, number.length / 2));
+            List<String> secondHalfReversed = Arrays.asList(Arrays.copyOfRange(number, number.length / 2 + 1, number.length));
+            Collections.reverse(secondHalfReversed);
+            return firstHalf.equals(secondHalfReversed);
+        }
+    }
+
+
 }
