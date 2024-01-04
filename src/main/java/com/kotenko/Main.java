da231de05d258944229d6a470bfe422469122f8b
Alex <akotenko@prezna.com>
2024-01-03 23:34:54 -0500

    0201L_first_tasks: added check

--- src/main/java/com/kotenko/Main.java
+++ src/main/java/com/kotenko/Main.java
@@ -45,5 +45,20 @@ public class Main {
         }
     }
 
-
+    public boolean check(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int curr = nums[left] + nums[right];
+            if (curr == target) {
+                return true;
+            }
+            if (curr > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return false;
+    }
 }
