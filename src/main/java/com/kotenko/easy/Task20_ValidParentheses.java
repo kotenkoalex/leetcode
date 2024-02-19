c26854235c1aad6ab6b9cfbada7560e5ef4d8055
Alex <akotenko@prezna.com>
2024-02-19 15:25:49 -0500

    0411L_easy: Task20_ValidParentheses solved

--- src/main/java/com/kotenko/easy/Task20_ValidParentheses.java
+++ src/main/java/com/kotenko/easy/Task20_ValidParentheses.java
@@ -1,7 +1,30 @@
 package com.kotenko.easy;
 
+import java.util.Stack;
+
+//https://leetcode.com/problems/valid-parentheses/
 public class Task20_ValidParentheses {
     public static void main(String[] args) {
+        System.out.println(isValid("()[]{}"));
+    }
 
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 }
