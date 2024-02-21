d4bf2c9b141e7914e7ae2b2f5e93f7a826541409
Alex <akotenko@prezna.com>
2024-02-20 22:28:44 -0500

    0411L_easy: Task21_MergeTwoSortedLists solved

--- src/main/java/com/kotenko/easy/Task21_MergeTwoSortedLists.java
+++ src/main/java/com/kotenko/easy/Task21_MergeTwoSortedLists.java
@@ -1,7 +1,66 @@
 package com.kotenko.easy;
 
+//https://leetcode.com/problems/merge-two-sorted-lists/
 public class Task21_MergeTwoSortedLists {
-    public static void main(String[] args) {
+    static class ListNode {
+        int val;
+        ListNode next;
 
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
+
+    static class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode dummyHead = new ListNode(0);
+            ListNode current = dummyHead;
+
+            while (list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    current.next = list1;
+                    list1 = list1.next;
+                } else {
+                    current.next = list2;
+                    list2 = list2.next;
+                }
+                current = current.next;
+            }
+
+            if (list1 != null) {
+                current.next = list1;
+            } else {
+                current.next = list2;
+            }
+
+            return dummyHead.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        Solution solution = new Solution();
+        ListNode mergedList = solution.mergeTwoLists(list1, list2);
+
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " ");
+            mergedList = mergedList.next;
+        }
+    }
+
 }
