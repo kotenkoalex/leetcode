728af15a8dfa7d2702cd06edc4b11002eec0ba0f
Alex <akotenko@prezna.com>
2024-02-21 21:09:49 -0500

    0411L_easy: Task26_RemoveDuplicatesFromSortedArray solved

--- src/main/java/com/kotenko/easy/Task26_RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/kotenko/easy/Task26_RemoveDuplicatesFromSortedArray.java
@@ -1,7 +1,24 @@
 package com.kotenko.easy;
 
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 public class Task26_RemoveDuplicatesFromSortedArray {
-    public static void main(String[] args) {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int uniqueCount = 1;
+        int currentIndex = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[currentIndex] = nums[i];
+                currentIndex++;
+                uniqueCount++;
+            }
+        }
+        return uniqueCount;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{1, 1, 2}));
     }
 }
