e734c4980052331ec7b937f212dfd4134fd48865
Alex <akotenko@prezna.com>
2024-02-23 20:54:57 -0500

    0411L_easy: Task28_FindTheIndexOfTheFirstOccurrenceInAString solved

--- src/main/java/com/kotenko/easy/Task28_FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/main/java/com/kotenko/easy/Task28_FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,7 +1,27 @@
 package com.kotenko.easy;
 
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 public class Task28_FindTheIndexOfTheFirstOccurrenceInAString {
-    public static void main(String[] args) {
 
+    public static int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j;
+            for (j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+            }
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(strStr("sadbutsad", "sad"));
     }
 }
