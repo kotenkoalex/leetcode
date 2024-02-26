e07c7ab6c032d0ceedfad172522dc65c3a2fc5ef
Alex <akotenko@prezna.com>
2024-02-26 15:19:55 -0500

    0411L_easy: Task66_PlusOne solved

--- src/main/java/com/kotenko/easy/Task66_PlusOne.java
+++ src/main/java/com/kotenko/easy/Task66_PlusOne.java
@@ -1,7 +1,28 @@
 package com.kotenko.easy;
 
-public class Task66_PlusOne {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+//https://leetcode.com/problems/plus-one/
+public class Task66_PlusOne {
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            digits[i]++;
+            if (digits[i] == 10) {
+                digits[i] = 0;
+            } else {
+                break;
+            }
+        }
+        if (digits[0] == 0) {
+            int[] result = new int[n + 1];
+            result[0] = 1;
+            return result;
+        }
+        return digits;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(plusOne(new int[]{1, 2, 3})));
     }
 }
