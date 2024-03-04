3436b27f11fcb2f5a18464cc0280c67ae248747c
Alex <akotenko@prezna.com>
2024-03-03 21:20:13 -0500

    0411L_easy: Task88_MergeSortedArray solved

---
+++ src/main/java/com/kotenko/easy/Task88_MergeSortedArray.java
@@ -0,0 +1,29 @@
+package com.kotenko.easy;
+
+//https://leetcode.com/problems/merge-sorted-array/
+public class Task88_MergeSortedArray {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
+        }
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
+    }
+
+    public static void main(String[] args) {
+        merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
+    }
+}
