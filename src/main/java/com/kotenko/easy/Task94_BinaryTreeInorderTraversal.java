5d56d9d6242791489d58d921ae7b3f902bfa990c
Alex <akotenko@prezna.com>
2024-03-05 18:34:45 -0500

    0411L_easy: added Task94_BinaryTreeInorderTraversal

---
+++ src/main/java/com/kotenko/easy/Task94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,50 @@
+package com.kotenko.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+//https://leetcode.com/problems/binary-tree-inorder-traversal/
+public class Task94_BinaryTreeInorderTraversal {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        inorderTraversal(root);
+    }
+}
