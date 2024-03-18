b214f41e4b0571d45f1e6643ee3313221486acf8
Alex <akotenko@prezna.com>
2024-03-18 15:25:19 -0400

    0411L_easy: added Task111_MinimumDepthOfBinaryTree

---
+++ src/main/java/com/kotenko/easy/Task111_MinimumDepthOfBinaryTree.java
@@ -0,0 +1,36 @@
+package com.kotenko.easy;
+
+//https://leetcode.com/problems/minimum-depth-of-binary-tree/
+public class Task111_MinimumDepthOfBinaryTree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public class MinimumDepthOfBinaryTree {
+        public int minDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            if (root.left == null && root.right == null) {
+                return 1;
+            }
+            if (root.left == null) {
+                return 1 + minDepth(root.right);
+            }
+            if (root.right == null) {
+                return 1 + minDepth(root.left);
+            }
+            return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
+}
